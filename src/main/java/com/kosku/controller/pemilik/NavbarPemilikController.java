8da736ec4fecfd73d735aba42a15076c05b13da8
Wahyu Pratama <wahyupratama110107@gmail.com>
2026-04-29 18:09:09 +0700

    Refactor DAO and model classes for improved query handling and data integrity

--- src/main/java/com/kosku/controller/pemilik/NavbarPemilikController.java
+++ src/main/java/com/kosku/controller/pemilik/NavbarPemilikController.java
@@ -1,7 +1,6 @@
 package com.kosku.controller.pemilik;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Node;
